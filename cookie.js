81cf82ed90fe4d1e704e65ca8569cf037a813bb4
Andreas Stopat <dev@demonsgalore.de>
2019-02-06 10:30:07 +0100

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,14 +1,19 @@
-var expireDate = new Date();
-var time = expireDate.getTime();
-var expireTime = time + 2592000000;
+
+
+let expireDate = new Date();
+let time = expireDate.getTime();
+let expireTime = time + 2592000000;
 expireDate.setTime(expireTime);
 
-function getCookie(name) {
-  var value = "; " + document.cookie;
-  var parts = value.split("; " + name + "=");
+getCookie(name) => {
+  let value = "; " + document.cookie;
+  let parts = value.split("; " + name + "=");
   if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
 if (getCookie('name')) {
   
 }
+
+
+document.cookie = 'name=' + value + '; expires=' + expireDate.toGMTString() + ';path=/';
