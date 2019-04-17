3b81d55b89781561409c378874014fed27eb0d79
Andreas Stopat <dev@demonsgalore.de>
2019-04-17 08:34:48 +0200

    Update dates.js

--- dates.js
+++ dates.js
@@ -6,3 +6,6 @@ let date = new Date(); // new Date(unformattedDate)
 
 2digit month
 ("0" + (date.getMonth() + 1)).slice(-2)
+
+const date = new Date(dateInputString);
+const dateString = ("0" + date.getDate()).slice(-2) + '.' + ("0" + (date.getMonth() + 1)).slice(-2) + '.' + date.getFullYear();
