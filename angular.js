d5138e19d5f68e8fb555e89fa562da4f6da3fbae
Andreas Stopat <dev@demonsgalore.de>
2019-09-29 12:29:50 +0200

    Create angular.js

---
+++ angular.js
@@ -0,0 +1,11 @@
+npm install -g @angular/cli
+npm install -g typescript
+ng new project-name
+
+
+ng generate component components/Todos
+ng g c components/TodoItem
+ng generate service services/Todo
+
+
+ng dist -> push /dist to webhost
